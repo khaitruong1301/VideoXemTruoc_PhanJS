aaffb0902d7e5eebde9490adca8873ca800cc45c
macbook <khaitruong1301@gmail.com>
2022-07-12 13:43:30 +0700

    video8_vidu2

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
 /*
     Viết chương trình cho phép người dùng nhập vào 1 số => tính tổng các kí số của số đó
     Ví dụ: 585 => 5 + 8 + 5 = 18
+    // 585/100 = 5.85 => Math.floor
+    // 585 % 100 = 85 => 85 / 10 = 8.5 => làm tròn 8
 */
 
 //input
-var so = 585;
+var so = 876;
 
 //output: string ? boolean ? number ?
 var tongKySo = 0;
 
 //progress: Xử lý
 var hangTram = Math.floor(so / 100);
-var hangChuc = Math.floor(so % 100 / 10); //585 % 100 => 85 / 10 => 8.5 => làm tròn xuống Math.floor(8.5)=>8
-var hangDonVi = so % 10; // 585 % 10 = 58 dư 5
+var hangChuc = Math.floor(so % 100 / 10 );
+var donVi = so % 100 % 10;
 
-tongKySo = hangTram + hangChuc + hangDonVi;
+tongKySo = hangTram + hangChuc + donVi;
 
-console.log('tổng ký số', tongKySo);
+console.log('Tổng ký số = ', tongKySo);
+
+
